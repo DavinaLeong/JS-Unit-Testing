993f885b6c4e12ba2012a71da25e71ed1766be94
Davina Leong <davina_leong@theshipyard.sg>
2017-11-30 10:18:58 +0800

    Refining unit tests.

--- test/reverse.test.js
+++ test/reverse.test.js
@@ -1,14 +1,18 @@
-let reverse = require('../lib/reverse');
-let expect = require('chai').expect;
+let reverse = require('../lib/reverse')
+let expect = require('chai').expect
 
-describe("#reverse", () => {
-    it("String reversed", () => {
-        expect(revese("hello").to.equal("olleh"));
-    });
-});
+describe("#reverse", function() {
+    it("String reversed", function() {
+        expect(reverse("hello")).to.equal("olleh");
+    })
 
-describe("#reverse-dash", () => {
-    it("String reversed", () => {
-        expect(revese("hello", "-").to.equal("o-l-l-e-h"));
-    });
-});
+    it("String reversed", function() {
+        expect(reverse("hello", "-")).to.equal("o-l-l-e-h")
+    })
+})
+
+// describe("#reverse-hello-dash", function() {
+//     it("String reversed", function() {
+//         expect(reverse("hello", "-")).to.equal("o-l-l-e-h")
+//     })
+// })
